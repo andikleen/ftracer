4d0dada30e06f3c5c3a0333e2a7821fb6d725ce1
Andi Kleen <ak@linux.intel.com>
2013-06-09 11:12:03 -0700

    test program changes

--- test.c
+++ test.c
@@ -7,7 +7,7 @@ void f3(int a, int b, int c)
 	mb();
 }
 
-void f2(int a, int b, int c)
+void f2(char *a, int b, int c)
 {
 	f3(1, 2, 3);
 	f3(4, 5, 6);
@@ -15,8 +15,8 @@ void f2(int a, int b, int c)
 
 void f1(int a, int b, int c)
 {
-	f2(10, 11, 12);
-	f2(20, 21, 22);
+	f2("10", 11, 12);
+	f2("20", 21, 22);
 	f3(7, 8, 9);
 }
 
