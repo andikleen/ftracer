d14c86277ef258c86477e27cf2aeea3adabe819c
Andi Kleen <ak@linux.intel.com>
2013-06-09 11:12:03 -0700

    fix dumper

--- ftracer.c
+++ ftracer.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dlfcn.h>
+#include <string.h>
 
 #include "ftracer.h"
 
@@ -55,6 +56,8 @@ static volatile bool tenabled;
 static __thread struct trace tbuf[TSIZE];
 static int tcur;
 
+static double frequency;
+
 __attribute__((used)) void ftracer(struct frame *fr)
 {
 	if (!tenabled)
@@ -70,17 +73,21 @@ __attribute__((used)) void ftracer(struct frame *fr)
 	t->arg3 = fr->rdx;
 }
 
-void ftrace_enable(void)
+bool ftrace_enable(void)
 {
+	bool old = tenabled;
 	tenabled = true;
+	return old;
 }
 
-void ftrace_disable(void)
+bool ftrace_disable(void)
 {
+	bool old = tenabled;
 	tenabled = false;
+	return old;
 }
 
-static char *resolve(char *buf, int buflen, uint64_t addr)
+static const char *resolve(char *buf, int buflen, uint64_t addr)
 {
 	Dl_info info;
 	if (dladdr((void *)addr, &info))
@@ -89,30 +96,50 @@ static char *resolve(char *buf, int buflen, uint64_t addr)
 	return buf;
 }
 
+static unsigned dump_start(unsigned max, unsigned cur)
+{
+	if (!max)
+		max = TSIZE;
+	if (tbuf[cur].tstamp) { /* Did it wrap? */
+		if (max >= TSIZE)
+			return (cur + 1) % TSIZE;
+		if (max > cur)
+			return (TSIZE - 1 - (max - (cur - max))); // XXX
+	} else { 
+		if (max >= cur)
+			return 0;
+	}
+	return cur - max;
+}
+
 void ftrace_dump(unsigned max)
 {
-	int num = 0;
 	struct trace *t;
 	int cur = tcur;
-	int i = cur - 1;
+	int i;
+	uint64_t ts = 0, last = 0;
+	bool oldstate = ftrace_disable();
 
-	do {
-		if (i < 0)
-			i = TSIZE-1;
+	for (i = dump_start(max, cur); i != cur; i = (i + 1) % TSIZE) {
 		t = &tbuf[i];
-		i--;
 		if (t->tstamp == 0)
 			break;
-
+		if (!ts)
+			ts = t->tstamp;
+		if (!last)
+			last = t->tstamp;
 		char src[128], dst[128];
-		printf("%lx %s->%s %lx %lx %lx\n",
-				t->tstamp,
-				resolve(src, sizeof src, t->src),
-				resolve(dst, sizeof dst, t->dst),
-				t->arg1, t->arg2, t->arg3);
-		
-		num++;
-	} while (i != cur && !(max && num < max));
+		printf("%6.1f %6.1f %-20s -> %-20s %lx %lx %lx\n",
+		       (t->tstamp - ts) / frequency,
+		       (t->tstamp - last) / frequency,
+		       resolve(src, sizeof src, t->src),
+		       resolve(dst, sizeof dst, t->dst),
+		       t->arg1, t->arg2, t->arg3);
+		last = t->tstamp;
+	}
+
+	if (oldstate)
+		ftrace_enable();
 }
 
 static int dump_at_exit;
@@ -130,4 +157,51 @@ void ftrace_dump_at_exit(unsigned max)
 	setup = true;
 	dump_at_exit = max;
 	atexit(call_ftrace_dump);
+}
+
+static void __attribute__((constructor)) init_ftracer(void)
+{
+     FILE *f = fopen("/proc/cpuinfo", "r");
+     if (!f)
+	  goto fallback;
+
+     char *line = NULL;
+     size_t linelen = 0;
+     frequency = 0;
+     while (getline(&line, &linelen, f) > 0) {
+	  char unit[10];
+
+	  if (strncmp(line, "model name", sizeof("model name")-1))
+	       continue;
+	  if (sscanf(line + strcspn(line, "@") + 1, "%lf%10s", 
+		     &frequency, unit) == 2) {
+	       if (!strcasecmp(unit, "GHz"))
+		     ;
+	       else if (!strcasecmp(unit, "Mhz"))
+		     frequency *= 1000.0;
+	       else {
+		    printf("Cannot parse unit %s\n", unit);
+		    goto fallback;
+	       }
+	       break;
+	  }
+     }     
+     free(line);
+     fclose(f);
+     if (frequency)
+	  return;
+    
+fallback:
+     f = fopen("/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq", "r");
+     int found = 0;
+     if (f) {
+         found = fscanf(f, "%lf", &frequency);
+	 fclose(f);
+     }
+     if (found == 1) {
+         frequency /= 1000000.0;
+         return;
+     }
+     printf("Cannot find frequency\n");
+     frequency = 1;
 }
--- ftracer.h
+++ ftracer.h
@@ -1,8 +1,8 @@
 #ifndef FTRACER_H
 #define FTRACER_H 1
 
-void ftrace_enable(void);
-void ftrace_disable(void);
+bool ftrace_enable(void);
+bool ftrace_disable(void);
 void ftrace_dump(unsigned max);
 void ftrace_dump_at_exit(unsigned max);
 
